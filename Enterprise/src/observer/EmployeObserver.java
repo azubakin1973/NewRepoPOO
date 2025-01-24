9ba0a83432eae54a31d52e3a407bd8710c964425
azubakin1973 <58044600+azubakin1973@users.noreply.github.com>
2025-01-24 16:57:38 -0500

    Delete Enterprise directory

--- Enterprise/src/observer/EmployeObserver.java
+++
@@ -1,9 +0,0 @@
-package observer;
-
-import model.Employe;
-
-public interface EmployeObserver {
-    void employeAjoute(Employe employe);
-    void employeModifie(Employe employe);
-    void employeSupprime(int id);
-}
--- Enterprise/src/observer/StatistiquesObserver.java
+++
@@ -1,5 +0,0 @@
-package observer;
-
-public interface StatistiquesObserver {
-    void statistiquesUpdated();
-}
